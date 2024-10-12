9684dc17aafc066c1659d68fd24327292f5ff626
satorres_8 <sergioavilatorres08@gmail.com>
2024-10-12 21:48:24 +0200

    RestaurantMenu

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,16 @@
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $30', 'Pasta - $25', 'Burger - $20', 'Salmon - $28'];
+const dessertMenu = ['Cake - $10', 'Ice Cream - $8', 'Pudding - $7', 'Fruit Salad - $9'];
+
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+    mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
